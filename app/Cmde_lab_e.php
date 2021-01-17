2f791c9a1cfdbb7a50fef46165a2b729eac4e38e
iheb <ihebsaad@gmail.com>
2021-01-17 21:02:49 +0100

    fix error

--- app/Cmde_lab_e.php
+++ app/Cmde_lab_e.php
@@ -6,7 +6,7 @@ class Cmde_lab_e extends Model
 {
 protected $table='cmde_lab_e';
 protected $primaryKey = 'cmde_lab_ident';
-protected $fillable = ['cl_ident','cmde_lab_date','cmde_lab_canal','cmde_lab_qte','cmde_lab_poids' ];
+protected $fillable = ['cl_ident','cmde_lab_date','cmde_lab_canal','cmde_lab_qte','cmde_lab_poids','statut' ];
   
   
   
