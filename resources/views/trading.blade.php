e84e00a539c22b0b7c682a03a3c9026835aacb77
iheb <ihebsaad@gmail.com>
2021-02-04 22:34:42 +0100

    style

--- resources/views/trading.blade.php
+++ resources/views/trading.blade.php
@@ -23,13 +23,16 @@ td {padding:8px 8px 8px 8px;}
  border:#000 1px solid;
 }				
 
+.separationTime{
+ border-left:#000 2px dashed;
+}
 .separationTime ,.separation{
-  border-right:#000 2px dashed;
+ border-bottom:#000 2px dashed;
+ border-right:#000 2px dashed;
  } 
 
-.separation0,.separationTime  {
+.separation0 {
  border-bottom:#000 2px dashed;
- border-left:#000 2px dashed;
  }
 </style>								
 								
