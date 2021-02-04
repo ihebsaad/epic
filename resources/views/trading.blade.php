55745d719701435b707fb6fd8ece99caa7c26ab4
iheb <ihebsaad@gmail.com>
2021-02-04 20:42:54 +0100

    fix

--- resources/views/trading.blade.php
+++ resources/views/trading.blade.php
@@ -5,7 +5,7 @@
  <div class="row">
 
                         <!-- Content Column -->
-                        <div class="col-lg-6 mb-4">
+                        <div class="col-lg-12 mb-4">
 
                             <!-- Project Card Example -->
                             <div class="card shadow mb-4">
@@ -13,6 +13,10 @@
                                     <h6 class="m-0 font-weight-bold text-primary">Trading</h6>
                                 </div>
                                 <div class="card-body">
+<style>	
+table{border:none;}							
+</style>								
+								
 <!--
  <style TYPE="text/css">  
 
