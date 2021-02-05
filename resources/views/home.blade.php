17a585b6f60acfddae1f774be7d0dd778c2d7e90
iheb <ihebsaad@gmail.com>
2021-02-05 16:39:10 +0100

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\HomeController ;
 
  $commandes=HomeController::commandes_ac($user['client_id'] );
   $modeles=HomeController::modeles_ac($user['client_id'] );
- $euro=HomeController::compte_euro($user['client_id'] );
- // $poids=HomeController::compte_poids($user['client_id'] );
+ $euros=HomeController::compte_euro($user['client_id'] );
+   $poids=HomeController::compte_poids($user['client_id'] );
  
   $natures=HomeController::natures2( );
  $Natures=array();
@@ -71,7 +71,7 @@ foreach($natures as $nature)
 				<td class="text-center"><?php echo  date('d/m/Y', strtotime($cmd->date_cmde)); ?></td>	
 				<td class="text-center"><?php echo $cmd->qte; ?></td>	
 				<td class="text-center"><?php echo $cmd->poids; ?>g</td>	
-				<td class="text-center"><?php if($cmd->facon>0){echo $cmd->facon.' €';} ?></td>	
+				<td class="text-center"><?php if($cmd->facon>0){echo $cmd->facon.'€';} ?></td>	
 				<td class="text-center"><?php echo $cmd->type_cmde; ?></td>	
 			</tr>	
 			<?php }  ?>	
@@ -128,15 +128,15 @@ foreach($natures as $nature)
                                     <h6 class="m-0 font-weight-bold text-primary">{{__('msg.My templates')}}</h6>
 									</a>
                                 </div>
-                                <div id="div2" class="card-body">
+                                <div id="div2" class="card-body collapse">
  
          <table   class="table table-striped mb-40"  style="width:100%">
             <thead>
             <tr id="headtable">
-                <th class="text-center">{{__('msg.Type')}}</th>
-                <th class="text-center">{{__('msg.Name')}}</th>
-                <th class="text-center"  >{{__('msg.Nature')}}</th>
-                <th class="text-center">{{__('msg.Weight')}}</th>
+                <th class="text-center" style="font-size: 13px;">{{__('msg.Type')}}</th>
+                <th class="text-center" style="font-size: 13px;">{{__('msg.Name')}}</th>
+                <th class="text-center" style="font-size: 13px;" >{{__('msg.Nature')}}</th>
+                <th class="text-center" style="font-size: 13px;">{{__('msg.Weight')}}</th>
                 <th class="text-center"><small>{{__('msg.Gold')}}</small></th>
                 <th class="text-center"><small>{{__('msg.Silver')}}</small></th>
                 <th class="text-center"><small>{{__('msg.Platinum')}}</small></th>
@@ -148,7 +148,7 @@ foreach($natures as $nature)
 			<tr>
 				<td class="text-center"><small><?php echo  $modele->metier ; ?></small></td>	
 				<td class="text-center"><small><?php echo $modele->nom; ?></small></td>	
- 				<td class="text-center"><small><?php   echo $Natures[$modele->nature]; ?></small></td>	
+ 				<td class="text-center"><small><?php echo $Natures[$modele->nature]; ?></small></td>	
  				<td class="text-center"><small><?php echo  $modele->poids.'g' ; ?></small></td>	
  				<td class="text-center"><small><?php if($modele->AU>0) {echo  $modele->AU.'g';} ?></small></td>	
  				<td class="text-center"><small><?php if($modele->AG>0) {echo  $modele->AG.'g';} ?></small></td>	
@@ -167,9 +167,38 @@ foreach($natures as $nature)
 										<h6 class="m-0 font-weight-bold text-primary">{{__('msg.My Euros Account')}}</h6>
 									</a>
                                 </div>
-                                <div id="div3" class="card-body">
+                                <div id="div3" class="card-body collapse">
 
-								
+        <table   class="table table-striped mb-40"  style="width:100%">
+            <thead>
+            <tr id="headtable">
+                <th class="text-center;" style="font-size: 13px;">{{__('msg.Date')}}</th>
+                <th class="text-center;" style="font-size: 13px;">{{__('msg.Piece')}}</th>
+                <th class="text-center:" style="font-size: 13px;">{{__('msg.Label')}}</th>
+                <th class="text-center;" style="font-size: 13px;">Lettrage</th>
+				<th class="text-center;" style="font-size: 13px;">Echéance</th>	
+                <th class="text-center;" style="font-size: 13px;">{{__('msg.Debit')}}</th>
+                <th class="text-center;" style="font-size: 13px;">{{__('msg.Credit')}}</th>
+                <th class="text-center;" style="font-size: 13px;">{{__('msg.Balance')}}</th>
+               </tr>
+            </thead>
+            <tbody>
+            @foreach($euros as $euro)                                     
+			<tr style="font-size:12px;">
+			<?php if($euro->solde >= 0){$style="color:#54ba1d";}else{$style="color:#d03132";} ?>
+			
+				<td class="text-center" style=""><small><?php echo  date('d/m/Y', strtotime($euro->ecrit_date)); ?></small></td>	
+				<td class="text-center"><small><?php echo $euro->num_piece; ?></small></td>	
+				<td class="text-center" style=""><small ><?php echo $euro->libelle; ?></small></td>	
+				<td class="text-center;"><small><?php echo $euro->lettrage; ?></small></td>				
+				<td class="text-center" style=""><small><?php echo date('d/m/Y', strtotime($euro->echeance)); ?></small></td>	
+				<td class="text-center"><small><?php if($euro->debit > 0){ echo $euro->debit.'€'; }?></small></td>	
+				<td class="text-center"><small><?php  if($euro->credit > 0){ echo $euro->credit.'€';} ?></small></td>	
+				<td class="text-center" style="<?php echo $style; ?>"><small><?php echo $euro->solde.'€'; ?></small></td>	
+			</tr>	
+ 			@endforeach
+			</tbody>
+			</table>								
                                 </div>
                             </div>
 
