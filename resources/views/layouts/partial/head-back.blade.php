504fff6b48a87fd5b4b06902b34312c17bd4e558
iheb <ihebsaad@gmail.com>
2021-02-06 14:16:41 +0100

    test

--- resources/views/layouts/partial/head-back.blade.php
+++ resources/views/layouts/partial/head-back.blade.php
@@ -108,9 +108,7 @@
 	
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
-    if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
-    };	
+ 	
  }else{
 	// alert('desktop');
  }
