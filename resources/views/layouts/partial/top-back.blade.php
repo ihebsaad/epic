0c8ccd6701fb4c736b599d032dadee53185b8e76
iheb <ihebsaad@gmail.com>
2021-02-06 16:30:51 +0100

    update menu mobile

--- resources/views/layouts/partial/top-back.blade.php
+++ resources/views/layouts/partial/top-back.blade.php
@@ -99,7 +99,7 @@ app()->setLocale($lg);
 			</li>
  -->
             <!-- Nav Item - Alerts -->
-            <li class="nav-item dropdown no-arrow mx-1">
+            <li class="hidemobile nav-item dropdown no-arrow mx-1">
               <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas  fa-user-cog  fa-fw"></i>  <small style="color:darkgrey">{{__('msg.My Operations')}}</small>
                 <!-- Counter - Alerts --> 
@@ -180,68 +180,22 @@ $count_rmp =count($E_CmdesRMP);
 $count= $count_aff + $count_lab + $count_rmp;
 ?>
             <!-- Nav Item - Messages --> 
+ 
  			<li class="nav-item dropdown no-arrow mx-1">
-	              <a class="nav-link dropdown-toggle" href="{{route('panier')}}"  >
+              <a class="nav-link dropdown-toggle" href="{{route('panier')}}"  >
                 <i class="fas fa-shopping-cart fa-fw"></i>
                 <!-- Counter - Messages --> 
-                <span class="badge badge-danger badge-counter" data-toggle="tooltip" data-placement="bottom"  title="{{__('msg.Products')}}" ><?php if($prods>0){echo $prods;}?></span>
-              </a>	
-			</li>
+                <span  data-toggle="tooltip" data-placement="bottom" title="{{__('msg.Products')}}" class="badge badge-danger badge-counter"><?php if($prods>0){echo $prods;}?></span>
+              </a>
+	   
+            </li> 			
  			<li class="nav-item dropdown no-arrow mx-1">
               <a class="nav-link dropdown-toggle" href="{{route('livraisonmod')}}"  >
                 <i class="fas fa-shopping-bag fa-fw"></i>
                 <!-- Counter - Messages --> 
                 <span  data-toggle="tooltip" data-placement="bottom" title="{{__('msg.My templates')}}" class="badge badge-danger badge-counter"><?php if($count>0){echo $count;}?></span>
               </a>
-	  
-			  
-              <!-- Dropdown - Messages  
-              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                  Message Center
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div class="font-weight-bold">
-                    <div class="text-truncate">Hi there! I am wondering if you can help me with a problem I've been having.</div>
-                    <div class="small text-gray-500">Emily Fowler · 58m</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
-                    <div class="status-indicator"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">I have the photos that you ordered last month, how would you like them sent to you?</div>
-                    <div class="small text-gray-500">Jae Chun · 1d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
-                    <div class="status-indicator bg-warning"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Last month's report looks great, I am very happy with the progress so far, keep up the good work!</div>
-                    <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</div>
-                    <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                  </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-              </div>---->
+	   
             </li> 
  
             <div class="topbar-divider d-none d-sm-block"></div>
