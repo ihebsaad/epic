58263730546516e16a6af3e832bf7790d9db1bff
iheb <ihebsaad@gmail.com>
2021-02-18 17:49:42 +0100

    test

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Session;
 use App\User ;
+use GuzzleHttp\Client;
 
 class PagesController extends Controller
 {
@@ -27,7 +28,57 @@ class PagesController extends Controller
 	
 	  public function orders()
     { 
-         return view('orders');
+	
+	
+	$endpoint = "https://api-mock.dhl.com/mydhlapi/rates";
+$client = new \GuzzleHttp\Client();
+/*
+accountNumber=220136396&originCountryCode=FR&originPostalCode=75000&originCityName=Paris&destinationCountryCode=FR&
+destinationPostalCode=75001&destinationCityName=Paris&weight=5&length=15&width=10&height=5&plannedShippingDate=2020-02-26&isCustomsDeclarable=true&unitOfMeasurement=metric" -H  "accept: application/json" -H  "Message-Reference: d0e7832e-5c98-11ea-bc55-0242ac13" -H  "Message-Reference-Date: Wed, 21 Oct 2015 07:28:00 GMT" -H  "Plugin-Name:  " -H  "Plugin-Version:  " -H  "Shipping-System-Platform-Name:  " -H  "Shipping-System-Platform-Version:  " -H  "Webstore-Platform-Name:  " -H  "Webstore-Platform-Version:  " -H  "authorization: Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ="
+$accountNumber = 220136396;
+
+*/
+$originCountryCode = "FR";
+$originPostalCode = "75000";
+$originCityName = "Paris";
+$destinationCountryCode = "FR";
+$destinationPostalCode = "75001";
+$destinationCityName = "75001";
+$weight = 5;
+$length = 15;
+$width = 10;
+$height = 5;
+$plannedShippingDate = 2020-02-26;
+$isCustomsDeclarable = true;
+$unitOfMeasurement = "metric";
+ 
+
+
+$response = $client->request('GET', $endpoint, ['query' => [
+   /* 'key1' => $id, 
+    'key2' => $value,*/
+	
+'originCountryCode' =>$originCountryCode ,
+'originPostalCode' =>$originPostalCode  ,
+'originCityName' =>$originCityName  ,
+'destinationCountryCode' =>$destinationCountryCode  ,
+'destinationPostalCode' =>$destinationPostalCode  ,
+'destinationCityName' =>$destinationCityName  ,
+'weight' =>$weight ,
+'length' =>$length ,
+'width' =>$width ,
+'height' =>$height ,
+'plannedShippingDate' =>$plannedShippingDate ,
+'isCustomsDeclarable' =>$isCustomsDeclarable  ,
+'unitOfMeasurement' =>$unitOfMeasurement  ,
+]]);
+
+// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
+
+$statusCode = $response->getStatusCode();
+$content = $response->getBody();
+
+         return view('orders',['statusCode'=>$statusCode,'result'=>$content]);
     }
 	
 	  public function test()
--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -15,33 +15,17 @@
                             <!-- Project Card Example -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Result Request</h6>
                                 </div>
                                 <div class="card-body">
-                                     
+								<?php echo $result; ?>
                                 </div>
                             </div>
 
                        
 
                         </div>
-
-                        <div class="col-lg-6 mb-4">
-
-                            <!-- Illustrations -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                                </div>
-                                <div class="card-body">
  
- 
-                                </div>
-                            </div>
-
-               
-
-                        </div>
                     </div>
 
 @endsection
