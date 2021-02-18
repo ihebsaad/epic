4a051677c02ce38fd4541f81b4acc6267f5f9c02
iheb <ihebsaad@gmail.com>
2021-02-18 18:36:48 +0100

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,15 +10,15 @@
 						<div class="row">
 
                         <!-- Content Column -->
-                        <div class="col-lg-6 mb-4">
+                        <div class="col-lg-12 mb-4">
 
                             <!-- Project Card Example -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Result Request</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Tests</h6>
                                 </div>
                                 <div class="card-body">
-								<?php echo $result; ?>
+								<?php echo  json_encode($result, JSON_PRETTY_PRINT); ?>
                                 </div>
                             </div>
 
