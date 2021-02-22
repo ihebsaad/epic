96f6b36e7e3f90f2a7bb5e8821d9bbc14905869e
iheb <ihebsaad@gmail.com>
2021-02-22 16:35:06 +0100

    test

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -30,7 +30,79 @@ class PagesController extends Controller
     { 
 	
 	
-	$endpoint = "https://express.api.dhl.com/mydhlapi/test";
+	$endpoint = "https://api-mock.dhl.com/mydhlapi/rates";
+$client = new \GuzzleHttp\Client();
+/*
+accountNumber=220136396&originCountryCode=FR&originPostalCode=75000&originCityName=Paris&destinationCountryCode=FR&
+destinationPostalCode=75001&destinationCityName=Paris&weight=5&length=15&width=10&height=5&plannedShippingDate=2020-02-26&isCustomsDeclarable=true&unitOfMeasurement=metric" -H  "accept: application/json" -H  "Message-Reference: d0e7832e-5c98-11ea-bc55-0242ac13" -H  "Message-Reference-Date: Wed, 21 Oct 2015 07:28:00 GMT" -H  "Plugin-Name:  " -H  "Plugin-Version:  " -H  "Shipping-System-Platform-Name:  " -H  "Shipping-System-Platform-Version:  " -H  "Webstore-Platform-Name:  " -H  "Webstore-Platform-Version:  " -H  "authorization: Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ="
+$accountNumber = 220136396;
+
+*/
+$originCountryCode = "FR";
+$originPostalCode = "75000";
+$originCityName = "Paris";
+$destinationCountryCode = "FR";
+$destinationPostalCode = "75001";
+$destinationCityName = "75001";
+$weight = 5;
+$length = 15;
+$width = 10;
+$height = 5;
+$plannedShippingDate = 2020-02-26;
+$isCustomsDeclarable = true;
+$unitOfMeasurement = "metric";
+ 
+
+$credentials = base64_encode('saampFR' .':' . 'A@0eV^1zW!3x' ) ;
+
+$response = $client->request('GET', $endpoint, ['query' => [
+   /* 'key1' => $id, 
+    'key2' => $value,*/
+	
+'originCountryCode' =>$originCountryCode ,
+'originPostalCode' =>$originPostalCode  ,
+'originCityName' =>$originCityName  ,
+'destinationCountryCode' =>$destinationCountryCode  ,
+'destinationPostalCode' =>$destinationPostalCode  ,
+'destinationCityName' =>$destinationCityName  ,
+'weight' =>$weight ,
+'length' =>$length ,
+'width' =>$width ,
+'height' =>$height ,
+'plannedShippingDate' =>$plannedShippingDate ,
+'isCustomsDeclarable' =>$isCustomsDeclarable  ,
+'unitOfMeasurement' =>$unitOfMeasurement  ,
+],
+	'headers' => [
+	'accept'=> 'application/json',
+	'Message-Reference'=> 'd0e7832e-5c98-11ea-bc55-0242ac13',
+	'Message-Reference-Date'=> 'Wed, 21 Oct 2015 07:28:00 GMT',
+	'Plugin-Name'=> '',
+	'Plugin-Version'=> '',
+	'Shipping-System-Platform-Name'=> '',
+	'Shipping-System-Platform-Version'=> '',
+	'Webstore-Platform-Name'=> '',
+	'Webstore-Platform-Version'=> '',
+	//'authorization'=> 'Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ=',
+	'authorization'=> 'Basic {'.$credentials.'}',
+	 
+  	]
+]);
+
+// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
+
+$statusCode = $response->getStatusCode();
+$content = $response->getBody();
+
+         return view('orders',['statusCode'=>$statusCode,'result'=>$content]);
+    }
+	
+	  public function test()
+    { 
+
+//	$endpoint = "https://express.api.dhl.com/mydhlapi/test/rates";
+		$endpoint = "https://api-mock.dhl.com/mydhlapi/rates";
+
 $client = new \GuzzleHttp\Client();
 /*
 accountNumber=220136396&originCountryCode=FR&originPostalCode=75000&originCityName=Paris&destinationCountryCode=FR&
@@ -54,8 +126,9 @@ $unitOfMeasurement = "metric";
  
 
 
-$response = $client->request('GET', $endpoint, [  /*'query' => [
-   
+$response = $client->request('GET', $endpoint, ['query' => [
+   /* 'key1' => $id, 
+    'key2' => $value,*/
 	
 'originCountryCode' =>$originCountryCode ,
 'originPostalCode' =>$originPostalCode  ,
@@ -83,201 +156,8 @@ $response = $client->request('GET', $endpoint, [  /*'query' => [
 	'Webstore-Platform-Version'=> '',
 	'authorization'=> 'Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ=',
 	 
-  	]*/ 
-	 'auth' => ['saampFR', 'A@0eV^1zW!3x']
-
-]);
-
-// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
-
-$statusCode = $response->getStatusCode();
-$content = $response->getBody();
-
-         return view('orders',['statusCode'=>$statusCode,'result'=>$statusCode]);
-    }
-	
-	  public function test()
-    { 
-
-//	$endpoint = "https://express.api.dhl.com/mydhlapi/test/rates";
-		$endpoint = "https://api-mock.dhl.com/mydhlapi/rates";
-
-$client = new \GuzzleHttp\Client();
-/*
-accountNumber=220136396&originCountryCode=FR&originPostalCode=75000&originCityName=Paris&destinationCountryCode=FR&
-destinationPostalCode=75001&destinationCityName=Paris&weight=5&length=15&width=10&height=5&plannedShippingDate=2020-02-26&isCustomsDeclarable=true&unitOfMeasurement=metric" -H  "accept: application/json" -H  "Message-Reference: d0e7832e-5c98-11ea-bc55-0242ac13" -H  "Message-Reference-Date: Wed, 21 Oct 2015 07:28:00 GMT" -H  "Plugin-Name:  " -H  "Plugin-Version:  " -H  "Shipping-System-Platform-Name:  " -H  "Shipping-System-Platform-Version:  " -H  "Webstore-Platform-Name:  " -H  "Webstore-Platform-Version:  " -H  "authorization: Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ="
-$accountNumber = 220136396;
-
-*/
-$originCountryCode = "FR";
-$originPostalCode = "75000"; //get
-$originCityName = "Paris";  //get
-$destinationCountryCode = "FR";
-$destinationPostalCode = "75000";  
-$destinationCityName = "Paris";
-$weight = 5;  // get
-$length = 15;  // get
-$width = 10;  // get
-$height = 5;  // get
-$date=date('Y-m-d', strtotime('+1 Weekday'));
- 
-//$plannedShippingDate = 2020-02-26; 
-$plannedShippingDate = $date; 
-
-$isCustomsDeclarable = true;
-$unitOfMeasurement = "metric";
- 
-
-$data='
-
-{
-  "plannedShippingDateAndTime": "2021-02-25T14:00:31GMT+01:00",     
-  "pickup": {
-    "isRequested": false,
-    "closeTime": "18:00",
-    "location": "reception",
-    "specialInstructions": [
-      {
-        "value": "please ring door bell",    
-        "typeCode": "TBD"
-      }
-    ],
- 
- 
-  },
-  "productCode": "D",				 
-   "accounts": [
-    {
-      "typeCode": "shipper",
-      "number": "123456789"   
-    }
-  ],
- 
- 
-  "customerDetails": {
-    "shipperDetails": {
-      "postalAddress": {         
-        "postalCode": "14800",    
-        "cityName": "Prague",   
-        "countryCode": "CZ",   
-        "provinceCode": "CZ",     
-        "addressLine1": "V Parku 2308/10",    
-        "addressLine2": "addres2",
-        "addressLine3": "addres3",
-        "countyName": "Central Bohemia"
-      },
-      "contactInformation": {            
-        "email": "that@before.de",
-        "phone": "+1123456789",
-        "mobilePhone": "+60112345678",
-        "companyName": "Company Name",
-        "fullName": "John Brew"
-      },
- 
-    },
-    "receiverDetails": {
-      "postalAddress": {      
-        "postalCode": "14800",   
-        "cityName": "Prague",     
-        "countryCode": "CZ",     
-        "provinceCode": "CZ",   
-        "addressLine1": "V Parku 2308/10",
-        "addressLine2": "addres2",
-        "addressLine3": "addres3",
-        "countyName": "Central Bohemia"
-      },
-      "contactInformation": {      
-        "email": "that@before.de",
-        "phone": "+1123456789",
-        "mobilePhone": "+60112345678",
-        "companyName": "Company Name",
-        "fullName": "John Brew"
-      },
- 
- 
-    }
- 
-  },
-  "content": {
-    "packages": [
-      {
-        "typeCode": "2BP",
-        "weight": 22.5,   
-        "dimensions": {    
-          "length": 15,
-          "width": 15,
-          "height": 40
-        },
-        "customerReferences": [
-          {
-            "value": "Customer reference",   
-            "typeCode": "CU"
-          }
-        ],
-        "identifiers": [
-          {
-            "typeCode": "shipmentId", 
-            "value": "1111111111"  
-          }
-        ],
-        "description": "Modèles SAAMP",
-         
-    
-       }
-    ],
-    "isCustomsDeclarable": true,
-    "description": "Livraion des modèles SAAMP",
-     "incoterm": "DAP",
-    "unitOfMeasurement": "metric"
-  },
- 
- 
- 
- 
- }
-
-';
-$client->setBody($data);
-
-$response = $client->request('GET', $endpoint, [
-        'content-type' => 'application/json',
-/*
-'query' => [
-    
-	/*
-'originCountryCode' =>$originCountryCode ,
-'originPostalCode' =>$originPostalCode  ,
-'originCityName' =>$originCityName  ,
-'destinationCountryCode' =>$destinationCountryCode  ,
-'destinationPostalCode' =>$destinationPostalCode  ,
-'destinationCityName' =>$destinationCityName  ,
-'weight' =>$weight ,
-'length' =>$length ,
-'width' =>$width ,
-'height' =>$height ,
-'plannedShippingDate' =>$plannedShippingDate ,
-'isCustomsDeclarable' =>$isCustomsDeclarable  ,
-'unitOfMeasurement' =>$unitOfMeasurement  , 
-
-],*/
-	'headers' => [
-	'accept'=> 'application/json',
-	'Content-Type'=> 'application/json',
-	'Message-Reference'=> 'd0e7832e-5c98-11ea-bc55-0242ac13',
-	'Message-Reference-Date'=> 'Wed, 21 Oct 2015 07:28:00 GMT',
-	'Plugin-Name'=> '',
-	'Plugin-Version'=> '',
-	'Shipping-System-Platform-Name'=> '',
-	'Shipping-System-Platform-Version'=> '',
-	'Webstore-Platform-Name'=> '',
-	'Webstore-Platform-Version'=> '',
-	'authorization'=> 'Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ=',
-	 
   	]
 ]);
-
-curl_setopt($ch, CURLOPT_PORT, $_SERVER['SERVER_PORT']);
-
 
 // url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
 
