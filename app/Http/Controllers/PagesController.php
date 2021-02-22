ef31227b48c849907d03189d79b0eeb940e79eda
iheb <ihebsaad@gmail.com>
2021-02-22 17:07:44 +0100

    test auth

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -85,13 +85,9 @@ $response = $client->request('GET', $endpoint, ['query' => [
 	'Webstore-Platform-Version'=> '',
 	//'authorization'=> 'Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ=',
 	//'authorization'=> 'Basic {'.$credentials.'}',
-	 
-  	], 'auth' => ['saampFR', 'A@0eV^1zW!3x']
-
-]
-
- 
- );
+	 'Authorization' => ['Basic'.base64_encode('saampFR'.':'.'A@0eV^1zW!3x')],         
+  	]
+]);
 
 // url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
 
