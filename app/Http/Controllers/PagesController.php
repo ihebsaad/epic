7e26ae1dcb87dfcd19a6274d542ed2706e5daa9e
iheb <ihebsaad@gmail.com>
2021-02-24 18:01:41 +0100

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Session;
+use DB;
 use App\User ;
 use GuzzleHttp\Client;
 use App\Http\Controllers\DHLController ;
