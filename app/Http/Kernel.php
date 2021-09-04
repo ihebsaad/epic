83d04e594340296b366321fed2a0ed721f0b393e
ihebsaad <ihebsaad@gmail.com>
2021-09-04 16:20:38 +0100

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 		 'client' => CheckClientCredentials::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
     ];
 }
