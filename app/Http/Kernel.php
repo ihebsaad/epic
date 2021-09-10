6c7b0965f3a068660af9cbb44ebf928b874e8dad
ihebsaad <ihebsaad@gmail.com>
2021-09-10 14:12:04 +0100

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 		 'client' => CheckClientCredentials::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
 
     ];
 }
