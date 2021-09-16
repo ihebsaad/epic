b53dc99a386487eae2764e7504ac49b1450672a0
ihebsaad <ihebsaad@gmail.com>
2021-09-16 19:57:18 +0100

    css

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/png" href="{{  URL::asset('public/img/favicon.png') }}">
 
         <title> SAAMP - ERROR</title>
 
@@ -63,7 +64,27 @@
                 margin-bottom: 30px;
             }
 			
+/*
+  ##Device = Tablets, Ipads (portrait)
+  ##Screen = B/w 768px to 1024px
+*/
 
+@media (min-width: 768px) and (max-width: 1024px) {
+ 
+ #errorimg{width:450px!important;}
+ 
+}
+
+/*
+  ##Device = Tablets, Ipads (landscape)
+  ##Screen = B/w 768px to 1024px
+*/
+
+@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+ 
+ #errorimg{width:450px!important;}
+ 
+}
 /*
   ##Device = Low Resolution Tablets, Mobiles (Landscape)
   ##Screen = B/w 481px to 767px
