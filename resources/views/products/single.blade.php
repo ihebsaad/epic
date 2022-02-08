2d2234a81bb07990af2f79bff670ef065730c865
ihebsaad <ihebsaad@gmail.com>
2022-02-08 17:04:13 +0100

    etat id

--- resources/views/products/single.blade.php
+++ resources/views/products/single.blade.php
@@ -242,9 +242,9 @@ foreach($compls as $c){
                                     <?php if($produit->choix_etat>0){ ?>
  
                                      
-                                    <select id="etat_id" class="form-control ml-10" placeholder="Etat"  style="width:120px;margin-left:80px" onchange="alert( $('etat_id').val() )">
-                                    <?php foreach($etats as $etat){?>
+                                    <select id="etat_id" class="form-control ml-10" placeholder="Etat"  style="width:120px;margin-left:80px" onchange="alert( $('etat_id').find('option:selected').val() )">
 									<option value="0"></option>
+									<?php foreach($etats as $etat){?>
 									<option value="<?php echo $etat->id;?>">
                                     <?php echo $etat->libelle;?> </option>
 									 <?php } ?> 
